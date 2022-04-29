ec319ae2e2d436c8c0b4ae2a4f550666ca51f993
Sahitya77 <sahityanijhawan@gmail.com>
2022-04-29 21:13:40 +0530

    hooks updated

---
+++ landing-page/src/components/home-section/champion/ChampionDetail.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useRef } from 'react'
+import './champion-detail.scss'
+import {closeIcon} from '../../../assets/images'
+
+const ChampionDetail=props=> {
+
+    const item=props.item
+    const iframeref=useRef(null)
+
+    useEffect(() => {
+        const height = iframeref.current.offsetWidth * 9 / 16 + 'px'
+        iframeref.current.setAttribute('height', height)
+    }, [])
+    const onClose = () => {
+        document.querySelector(`#champ-detail-${props.id}`).classList.remove('active')
+        iframeref.current.setAttribute('src', '')
+
+        const img = document.querySelector(`#champ-img-${props.id}`)
+        img.style.opacity = 0
+        setTimeout(() => {
+            img.remove()
+        }, 500);
+    }
+  return (
+    <div
+    id={`champ-detail-${props.id}`}
+    className={`champion-detail bg-image overlay ${props.active ? 'active' : ''}`}
+    style={{backgroundImage: `url(${item.bgLarge})`}}
+    >
+        <div className='champion-detail_content'>
+            <span>{item.nickName}</span>
+            <h2 className='name main-color'>{item.name}</h2>
+            <span>Role: <span className='second-color'>{item.role}</span> </span>
+            <span>Difficulty: <span className='second-color'>{item.difficulty}</span> </span>
+            <div className='story'>
+                {item.description}
+            </div>
+            <span>Champion spotlight</span>
+            <div className='video'>
+                <iframe
+                    title='champion spotlight'
+                    ref={iframeref}
+                    width="100%"
+                >
+                </iframe>
+            </div>
+        </div>
+        <div className='champion-detail_close' onClick={onClose}>
+            <img src={closeIcon} alt=""/>
+        </div>
+    </div>
+  )
+}
+
+export default ChampionDetail
--- landing-page/src/components/home-section/index.js
+++ landing-page/src/components/home-section/index.js
@@ -1,6 +1,8 @@
 import Welcome from "./welcome/Welcome";
 import Champion from "./champion/Champion";
+import ChampionDetail from "./champion/ChampionDetail";
 export{
     Welcome,
-    Champion
+    Champion,
+    ChampionDetail
 }
